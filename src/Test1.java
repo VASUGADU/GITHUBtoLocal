a02580fa8ff5d66f2efa4e754f90c48d2f4ab8df
Vasu <vasu.seelamneni@gmail.com>
2017-09-08 13:23:50 +0800

    Next commit

--- src/Test1.java
+++ src/Test1.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Test1 {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<String>();
+        names.add("Dan");
+        names.add("Simon");
+        names.add("SimAlvin");
+        names.add("Haikal");
+        names.add("Seelamneni");
+
+        names.
+    }
 }
